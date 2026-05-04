3de9deacf281802b830497d96d90e16ff60c5ba4
zamir.yusof <z4520ft@gmail.com>
2026-05-05 00:56:59 +0800

    switch to headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
-        headless: false,
+        headless: true,
         screenshot: 'only-on-failure',
         userAgent:
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
